6af0e7f2d426f2dcaf1398f4fea0ec011220dd46
Aayush <aayushsrivastava2012001@gmail.com>
2024-04-10 01:14:33 +0530

    partyOfCouples

---
+++ Easy/partyOfCouples.cpp
@@ -0,0 +1,16 @@
+/*
+Problem Link : https://www.geeksforgeeks.org/problems/alone-in-couple5507/1
+*/
+
+class Solution
+{
+public:
+    int findSingle(int n, int arr[])
+    {
+        // code here
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            ans ^= arr[i];
+        return ans;
+    }
+};
